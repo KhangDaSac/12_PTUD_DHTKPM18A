1fa51e7fdea2e22a8f39327db383fe9a9034472c
KhangDaSac <daophuckhang090@gmail.com>
2024-10-19 11:21:00 +0700

    commit

--- src/main/java/GUI/controllers/BanVeGUI.java
+++
@@ -1,8 +0,0 @@
-package GUI.controllers;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-
-public class BanVeGUI {
-
-}
---
+++ src/main/java/GUI/controllers/BanVe_GUI_Controller.java
@@ -0,0 +1,5 @@
+package GUI.controllers;
+
+public class BanVe_GUI_Controller {
+
+}
