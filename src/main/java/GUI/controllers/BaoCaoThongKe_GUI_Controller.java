ce9afd4edf79a87c6c7f4c2878558f032f869527
tongquockhanh2510 <22677951.khanh@student.iuh.edu.vn>
2024-11-04 08:43:49 +0700

    hoanthanhtranghoadon

--- src/main/java/GUI/controllers/BaoCaoThongKe_GUI_Controller.java
+++ src/main/java/GUI/controllers/BaoCaoThongKe_GUI_Controller.java
@@ -51,50 +51,123 @@ public class BaoCaoThongKe_GUI_Controller implements Initializable {
     private ComboBox cmbChonThang;
     @FXML
     private ComboBox cmbChonNam;
+    @FXML
+    private TextField txtSoVeBan;
+    @FXML
+    private TextField txtSoVeHuy;
+    @FXML
+    private TextField txtSoVeDoi;
 
 
+    public void thongKeDoanhThuTheoThang(int nam) {
+        try {
+            Connection con = ConnectDB.getInstance().getConnection();
+            String query = "exec thongKeDoanhThuTheoThangTrongNam ?";
+            PreparedStatement statement = con.prepareStatement(query);
+            statement.setInt(1, nam);
+            ResultSet rs = statement.executeQuery();
 
+            Double tongDoanhThu = 0.0;
+            int soVeDat = 0;
+            int soVeHuy = 0;
+            int soVeDoi = 0;
+            XYChart.Series<String, Double> series_01 = new XYChart.Series<>();
+            series_01.getData().add(new XYChart.Data<>("Jan", 0.0));
+            series_01.getData().add(new XYChart.Data<>("Feb", 0.0));
+            series_01.getData().add(new XYChart.Data<>("Mar", 0.0));
+            series_01.getData().add(new XYChart.Data<>("Apr", 0.0));
+            series_01.getData().add(new XYChart.Data<>("May", 0.0));
+            series_01.getData().add(new XYChart.Data<>("Jun", 0.0));
+            series_01.getData().add(new XYChart.Data<>("Jul", 0.0));
+            series_01.getData().add(new XYChart.Data<>("Aug", 0.0));
+            series_01.getData().add(new XYChart.Data<>("Sep", 0.0));
+            series_01.getData().add(new XYChart.Data<>("Oct", 0.0));
+            series_01.getData().add(new XYChart.Data<>("Nov", 0.0));
+            series_01.getData().add(new XYChart.Data<>("Dec", 0.0));
 
-    public static void main(String[] args) {
-        launch(args);
-       
-    }
-double tongDoanhThu=0;
-    public void thongKeDoanhThuTheoThang (LocalDate ngayBatDau, LocalDate ngayKetThuc) {
-            try {
-                Connection con = ConnectDB.getInstance().getConnection();
-                String query = "exec thongKeDoanhThuTheoThang ?,?";
-                PreparedStatement statement = con.prepareStatement(query);
-                statement.setDate(1, Date.valueOf(ngayBatDau));
-                statement.setDate(2, Date.valueOf(ngayKetThuc));
-                ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                String monthName = switch (rs.getInt("thang")) {
+                    case 1 -> "Jan";
+                    case 2 -> "Feb";
+                    case 3 -> "Mar";
+                    case 4 -> "Apr";
+                    case 5 -> "May";
+                    case 6 -> "Jun";
+                    case 7 -> "Jul";
+                    case 8 -> "Aug";
+                    case 9 -> "Sep";
+                    case 10 -> "Oct";
+                    case 11 -> "Nov";
+                    case 12 -> "Dec";
+                    default -> null;
+                };
+                if (monthName != null) {
+                    series_01.getData().removeIf(data -> data.getXValue().equals(monthName));
+                    series_01.getData().add(new XYChart.Data<>(monthName, rs.getDouble("monthly_revenue")));
 
-                while (rs.next()) {
-                    XYChart.Series<String, Double> series_01 = new XYChart.Series<>();
-                    switch (rs.getInt("thang")) {
-                        case 1 -> series_01.getData().add(new XYChart.Data("Jan", rs.getDouble("monthly_revenue")));
-                        case 2 -> series_01.getData().add(new XYChart.Data("Feb", rs.getDouble("monthly_revenue")));
-                        case 3 -> series_01.getData().add(new XYChart.Data("Mar", rs.getDouble("monthly_revenue")));
-                        case 4 -> series_01.getData().add(new XYChart.Data("Apr", rs.getDouble("monthly_revenue")));
-                        case 5 -> series_01.getData().add(new XYChart.Data("May", rs.getDouble("monthly_revenue")));
-                        case 6 -> series_01.getData().add(new XYChart.Data("Jun", rs.getDouble("monthly_revenue")));
-                        case 7 -> series_01.getData().add(new XYChart.Data("Jul", rs.getDouble("monthly_revenue")));
-                        case 8 -> series_01.getData().add(new XYChart.Data("Aug", rs.getDouble("monthly_revenue")));
-                        case 9 -> series_01.getData().add(new XYChart.Data("Sep", rs.getDouble("monthly_revenue")));
-                        case 10 -> series_01.getData().add(new XYChart.Data("Oct", rs.getDouble("monthly_revenue")));
-                        case 11 -> series_01.getData().add(new XYChart.Data("Nov", rs.getDouble("monthly_revenue")));
-                        case 12 -> series_01.getData().add(new XYChart.Data("Dec", rs.getDouble("monthly_revenue")));
-                    }
-                    tongDoanhThu = tongDoanhThu + rs.getDouble("monthly_revenue");
-                    chart.setLegendVisible(false);
-                    chart.getData().add(series_01);
+                    tongDoanhThu += rs.getDouble("monthly_revenue");
+                    soVeDat += rs.getInt("soVeBanDuoc");
+                    soVeHuy += rs.getInt("soVeHuy");
+                    soVeDoi += rs.getInt("soVeDoi");
                 }
-            } catch (SQLException  e) {
-                throw new RuntimeException(e);
             }
+            chart.getData().clear();
+            chart.setLegendVisible(false);
+            chart.getData().add(series_01);
+
+            txtDoanhThu.setText(String.format("%.2fM", tongDoanhThu / 1_000_000));
+            txtSoVeBan.setText(String.valueOf(soVeDat));
+            txtSoVeHuy.setText(String.valueOf(soVeHuy));
+            txtSoVeDoi.setText(String.valueOf(soVeDoi));
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+    }
+public void thongKeDoanhThuTheoNgay(int nam, int thang){
+   try{
+         Connection con = ConnectDB.getInstance().getConnection();
+         String query = "exec thongKeDoanhThuTheoNgayTrongThang ?,?";
+         PreparedStatement statement = con.prepareStatement(query);
+         statement.setInt(1, nam);
+         statement.setInt(2, thang);
+         ResultSet rs = statement.executeQuery();
 
+         Double tongDoanhThu = 0.0;
+         int soVeDat = 0;
+         int soVeHuy = 0;
+         int soVeDoi = 0;
+         XYChart.Series<String, Double> series_01 = new XYChart.Series<>();
 
+         // Khởi tạo dữ liệu mặc định cho các ngày với giá trị bằng 0
+         for (int i = 1; i <= 31; i++) {
+              series_01.getData().add(new XYChart.Data<>(String.valueOf(i), 0.0));
+         }
+
+         // Cập nhật dữ liệu từ ResultSet
+         while (rs.next()) {
+              int day = rs.getInt("ngay");
+              series_01.getData().removeIf(data -> data.getXValue().equals(String.valueOf(day)));
+              series_01.getData().add(new XYChart.Data<>(String.valueOf(day), rs.getDouble("daily_revenue")));
+             System.out.println("Ngày : " +  day + ", Doanh thu: " + rs.getDouble("daily_revenue"));
+             System.out.println("Ngày: " + day + ", Doanh thu: " + rs.getDouble("daily_revenue"));
+              tongDoanhThu += rs.getDouble("daily_revenue");
+              soVeDat += rs.getInt("soVeBanDuoc");
+              soVeHuy += rs.getInt("soVeHuy");
+              soVeDoi += rs.getInt("soVeDoi");
+         }
+         chart.getData().clear();
+         chart.setLegendVisible(false);
+         chart.getData().add(series_01);
+
+         txtDoanhThu.setText(String.format("%.2fM", tongDoanhThu / 1_000_000));
+         txtSoVeBan.setText(String.valueOf(soVeDat));
+         txtSoVeHuy.setText(String.valueOf(soVeHuy));
+         txtSoVeDoi.setText(String.valueOf(soVeDoi));
+   }catch (Exception e){
+       throw new RuntimeException(e);
+   }
+}
 
 
     public void initializeComboBoxes(){
@@ -108,16 +181,22 @@ double tongDoanhThu=0;
         cmbChonNam.setItems(nam);
     }
     public void btnThongKeOnAction(ActionEvent actionEvent) {
-        LocalDate ngayBatDau = dapNgayBatDau.getValue();
-        LocalDate ngayKetThuc = dapNgayKetThuc.getValue();
-        String loaiThoiGian = cmbLoaiThoiGian.getSelectionModel().getSelectedItem().toString();
-        String thang = cmbChonThang.getSelectionModel().getSelectedItem().toString();
-        int nam = Integer.parseInt(cmbChonNam.getSelectionModel().getSelectedItem().toString());
-        String loc = cmbLoc.getSelectionModel().getSelectedItem().toString();
-        chart.getData().clear();
-        tongDoanhThu = 0;
-        thongKeDoanhThuTheoThang(ngayBatDau, ngayKetThuc);
-        txtDoanhThu.setText(String.valueOf(tongDoanhThu/1000000)+"M");
+        if(cmbLoaiThoiGian.getValue().equals("Tháng")){
+            int nam = Integer.parseInt(cmbChonNam.getSelectionModel().getSelectedItem().toString());
+            chart.getData().clear();
+            thongKeDoanhThuTheoThang(nam);
+
+        }
+        else if(cmbLoaiThoiGian.getValue().equals("Ngày")){
+            if(cmbLoc.getValue().equals("Lọc theo tháng")){
+                int nam = Integer.parseInt(cmbChonNam.getSelectionModel().getSelectedItem().toString());
+                int thang = cmbChonThang.getSelectionModel().getSelectedIndex() + 1;
+                chart.getData().clear();
+                thongKeDoanhThuTheoNgay(nam,thang);
+            }
+
+        }
+
     }
 
     public void setMain_Controller(Main_Controller mainController) {
